f414fcbf37cc401f928ceb0c53774bca9c10f572
haruki7049 <tontonkirikiri@gmail.com>
2025-05-07 17:31:21 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use clap::Parser;
 use eqlang::parser as eqlang_parser;
-use eqlang::parser::Eqlang;
+use eqlang::parser::{Eqlang, Statement};
 
 fn main() -> anyhow::Result<()> {
     let args: CLIArgs = CLIArgs::parse();
 
-    let result: Eqlang = eqlang_parser::parse(&args.statement)?;
+    let mut result: Eqlang = eqlang_parser::parse(&args.statement)?;
 
-    dbg!(result.statements);
+    eval(&mut result)?;
+
+    Ok(())
+}
+
+/// Evaluates statements
+fn eval(eqlang: &mut Eqlang) -> anyhow::Result<()> {
+    eqlang.statements.reverse();
+
+    // Get the values
+    let cmd: Statement = eqlang.statements.pop().unwrap();
+    let first_argument: Statement = eqlang.statements.pop().unwrap();
+    let second_argument: Statement = eqlang.statements.pop().unwrap();
+
+    // Checks whether cmd is a command
+    if !cmd.is_command() {
+        panic!("EVAL ERROR: You must write a command, as '!!?!?!??' <- Plus");
+    }
+
+    // Addition
+    if cmd == Statement::Plus {
+        println!("{}", first_argument + second_argument);
+    }
 
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -11,14 +11,72 @@ pub struct Eqlang {
     pub statements: Vec<Statement>,
 }
 
-#[derive(Debug)]
+impl Statement {
+    /// Whether the statement is a command or not
+    pub fn is_command(self) -> bool {
+        self == Statement::Plus
+    }
+}
+
+impl std::ops::Add for Statement {
+    type Output = usize;
+
+    fn add(self, other: Self) -> Self::Output {
+        let first_number: Number = match self {
+            Statement::Plus => panic!(),
+            Statement::Number(v) => v,
+        };
+        let second_number: Number = match other {
+            Statement::Plus => panic!(),
+            Statement::Number(v) => v,
+        };
+
+        first_number + second_number
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Statement {
     Plus,
     Number(Number),
 }
 
-#[derive(Debug)]
+impl std::ops::Add for Number {
+    type Output = usize;
+
+    fn add(self, other: Self) -> Self::Output {
+        let first_value: usize = match self {
+            Number::Zero => 0,
+            Number::One => 1,
+            Number::Two => 2,
+            Number::Three => 3,
+            Number::Four => 4,
+            Number::Five => 5,
+            Number::Six => 6,
+            Number::Seven => 7,
+            Number::Eight => 8,
+            Number::Nine => 9,
+        };
+        let second_value: usize = match other {
+            Number::Zero => 0,
+            Number::One => 1,
+            Number::Two => 2,
+            Number::Three => 3,
+            Number::Four => 4,
+            Number::Five => 5,
+            Number::Six => 6,
+            Number::Seven => 7,
+            Number::Eight => 8,
+            Number::Nine => 9,
+        };
+
+        first_value + second_value
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Number {
+    Zero,
     One,
     Two,
     Three,
@@ -28,7 +86,6 @@ pub enum Number {
     Seven,
     Eight,
     Nine,
-    Ten,
 }
 
 fn parse_pair(pair: Pair<Rule>) -> anyhow::Result<Eqlang> {
@@ -44,7 +101,22 @@ fn parse_pair(pair: Pair<Rule>) -> anyhow::Result<Eqlang> {
             while let s = statement.as_span().as_str() {
                 match s {
                     "" => break,
+
+                    // +
                     "!!?!?!??" => result.statements.push(Statement::Plus),
+
+                    // 0~9
+                    "?????!!?" => result.statements.push(Statement::Number(Number::Zero)),
+                    "???!?!!?" => result.statements.push(Statement::Number(Number::One)),
+                    "??!??!!?" => result.statements.push(Statement::Number(Number::Two)),
+                    "??!!?!!?" => result.statements.push(Statement::Number(Number::Three)),
+                    "?!???!!?" => result.statements.push(Statement::Number(Number::Four)),
+                    "?!?!?!!?" => result.statements.push(Statement::Number(Number::Five)),
+                    "?!!??!!?" => result.statements.push(Statement::Number(Number::Six)),
+                    "?!!!?!!?" => result.statements.push(Statement::Number(Number::Seven)),
+                    "!????!!?" => result.statements.push(Statement::Number(Number::Eight)),
+                    "!??!?!!?" => result.statements.push(Statement::Number(Number::Nine)),
+
                     e => panic!("PARSE ERROR: Unknown word, {}", e),
                 }
 
